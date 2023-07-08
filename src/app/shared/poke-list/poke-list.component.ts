90557e45400b869d34b71498f34caed270002850
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-07-08 18:16:14 -0300

    adicionado o servico da api do pokemon

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
 
+  constructor(
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => res
+    );
+  }
 }
