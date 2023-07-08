fbac4f04cd156dc1de8637ec4717d7fef631e008
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-07-08 18:36:58 -0300

    adicionada a função para consumir a api de pokemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +16,22 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap( res => res ),
       tap( res => {
-        console.log(res)
+        res.results.map( (resPokemons: any )=> {
+
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+
+        })
       } )
     );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(
+        res => res
+      )
+    )
   }
 }
