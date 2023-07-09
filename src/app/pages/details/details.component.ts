4190f05761f9e45e6f3a05bfb39fe720eeb45f9c
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-07-09 14:08:48 -0300

    adicionado o scss do botão

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon() {
+    const id = this.activeRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      (res: any) => {
+        this.pokemon = res
+      }
+    )
+  }
 
 }
