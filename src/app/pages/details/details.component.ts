c87d21908c4af242ff4035a9b597213b9349e65c
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-07-09 14:51:06 -0300

    projeto finalizado

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { forkJoin } from 'rxjs';
+
+//Services
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
@@ -14,26 +16,31 @@ export class DetailsComponent implements OnInit {
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
   public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
 
   constructor(
-    private activeRoute: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon() {
-    const id = this.activeRoute.snapshot.params['id'];
+  public getPokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
-      (res: any) => {
-        this.pokemon = res
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
       }
-    )
+    );
   }
-
 }
